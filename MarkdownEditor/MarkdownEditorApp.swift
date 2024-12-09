98407ffebe0df7fbc566e1d2b6676af8f1ab57ac
WillowRussia <iv040606@gmail.com>
2024-12-09 22:41:54 +0300

    Initial Commit

---
+++ MarkdownEditor/MarkdownEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownEditorApp.swift
+//  MarkdownEditor
+//
+//  Created by Илья Востров on 09.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
